208ae9038d5da63802ab316f58b0973bef4237a9
Alakparov Ibragim <Alakp_mr11.itstep.edu.az>
2024-03-03 20:30:09 +0400

    comit usless thigs

--- src/components/CounterRedux/CounterRedux.tsx
+++ src/components/CounterRedux/CounterRedux.tsx
@@ -1,50 +1,31 @@
-<<<<<<< HEAD
-import { useTypedDispatch, useTypedSelector } from "../../store";
-import { increment, decrement } from "../../redux/slices/counterSlice";
 
+// import { increment, decrement} from "../redux/slices/CounterSlices";
+// import { increment as incrementStep,
+// 		 decrement as decrementStep } from "../redux/slices/StepSlices";
+// import { useTypedDispatch, useTypedSelector } from "../../store";
 
-const CounterRedux = () => {
+// const CounterRedux = () => {
+// 	const count = useTypedSelector(state => state.counterReducer);
+// 	const step = useTypedSelector(state => state.counterStepSlice);
+// 	const dispatch = useTypedDispatch();
 
-  const count = useTypedSelector(state => state.counterReducer)
-  const dispatch = useTypedDispatch();
-  return(
-    <>
-    <div>
-                <button onClick={()=> dispatch(decrement())}>-</button>
-                {count}
-                <button onClick={()=> dispatch(increment())}>+</button>
-            </div>
-    </>
-  )
-=======
-import { increment, decrement} from "../redux/slices/CounterSlices";
-import { increment as incrementStep,
-		 decrement as decrementStep } from "../redux/slices/StepSlices";
-import { useTypedDispatch, useTypedSelector } from "../../store";
-
-const CounterRedux = () => {
-	const count = useTypedSelector(state => state.counterReducer);
-	const step = useTypedSelector(state => state.counterStepSlice);
-	const dispatch = useTypedDispatch();
-
-	return (
-		<div>
-			<div>
-				<button onClick={() => dispatch(decrement(step))}>-</button>
-				{count}
-				<button onClick={() => dispatch(increment(step))}>+</button>
-			</div>
+// 	return (
+// 		<div>
+// 			<div>
+// 				<button onClick={() => dispatch(decrement(step))}>-</button>
+// 				{count}
+// 				<button onClick={() => dispatch(increment(step))}>+</button>
+// 			</div>
 			
-			<br/>
+// 			<br/>
 			
-			<div>
-				<button onClick={() => dispatch(decrementStep())}>-</button>
-				{step}
-				<button onClick={() => dispatch(incrementStep())}>+</button>
-			</div>
-		</div>
-	)
->>>>>>> 158e0bea8af56c2fe6de4c21ed8d70b5ffe6980e
-}
+// 			<div>
+// 				<button onClick={() => dispatch(decrementStep())}>-</button>
+// 				{step}
+// 				<button onClick={() => dispatch(incrementStep())}>+</button>
+// 			</div>
+// 		</div>
+// 	)
+// }
 
-export default CounterRedux;
+// export default CounterRedux;
