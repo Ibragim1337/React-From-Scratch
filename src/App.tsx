730d717eb884a6199e96e2870900400a70a5d450
Alakparov Ibragim <Alakp_mr11.itstep.edu.az>
2024-03-03 20:36:46 +0400

    timer task complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,11 @@
-import './App.css';
-import CounterRedux from './components/CounterRedux/CounterRedux';
-
-<<<<<<< HEAD
-import React from 'react';
-import CounterRedux from './components/CounterRedux/CounterRedux';
-import { useSelector } from 'react-redux';
+import Timer from "./components/Timer/Timer";
 
 
 export default function App() {
   
   return (
     <>
-      <CounterRedux />
+      <Timer />
     </>
   );
 }
-=======
-const App = () => {
-	return (
-		<>
-			<CounterRedux />
-		</>
-	);
-}
-
-export default App;
->>>>>>> 158e0bea8af56c2fe6de4c21ed8d70b5ffe6980e
